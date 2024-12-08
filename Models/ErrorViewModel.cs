91185c9b54d455e105f77d869b6f69f8b96f4fe0
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-12-08 10:56:18 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Memento_W.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
